4b06c94c66a42205623b5209b5ef2b055312c3c6
lopan <tjohander@gmail.com>
2021-08-31 00:49:27 -0500

    added data for info endpoint, changed start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cd /home/ec2-user/app/build/libs
-java -jar bloggerator.jar > /dev/null 2> /dev/null < /dev/null &
+cd /home/ec2-user/app
+#java -jar bloggerator.jar > /dev/null 2> /dev/null < /dev/null &
+./gradlew bootRun
