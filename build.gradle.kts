82cd2ce06286b1f6294d8fd2ab82793876fa39ab
lopan <tjohander@gmail.com>
2021-08-31 16:43:27 -0500

    Added Git Info to Actutor call

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.5.21"
     kotlin("plugin.spring") version "1.5.21"
+    id("com.gorylenko.gradle-git-properties") version "1.4.17"
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_11
