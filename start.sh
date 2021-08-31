e8e0de709507a7b3fb30075f875b242c072c7db8
lopan <tjohander@gmail.com>
2021-08-31 14:07:19 -0500

    update to start.sh and stop.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cd /home/ec2-user/app/build/libs
-nohup java -jar bloggerator.jar > /var/log/bloggerator.log 2>&1 &
+#cd /home/ec2-user/app
+#nohup java -jar bloggerator.jar > /var/log/bloggerator.log 2>&1 &
 #./gradlew bootRun
+sudo systemctl start bloggerator.service
